0bec3a3b1cb287b6b53a0d554f4a5cea952acd37
Reynaldo Navedo <codingimages@outlook.com>
2019-07-22 22:37:46 -0400

    Working on add pwa features, app manifest added

---
+++ js/scrollTo.js
@@ -0,0 +1,90 @@
+$("document").ready(function() {
+  $("a.about").click(function(e) {
+    e.preventDefault();
+    var link = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(link).offset().top
+      },
+      1000
+    );
+  });
+});
+
+$("document").ready(function() {
+  $("a.features").click(function(e) {
+    e.preventDefault();
+    var link = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(link).offset().top
+      },
+      1000
+    );
+  });
+});
+
+$("document").ready(function() {
+  $("a.pricing").click(function(e) {
+    e.preventDefault();
+    var link = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(link).offset().top
+      },
+      1000
+    );
+  });
+});
+
+$("document").ready(function() {
+  $("a.stories").click(function(e) {
+    e.preventDefault();
+    var link = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(link).offset().top
+      },
+      1000
+    );
+  });
+});
+
+$("document").ready(function() {
+  $("a.contact").click(function(e) {
+    e.preventDefault();
+    var link = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(link).offset().top
+      },
+      1000
+    );
+  });
+});
+
+$("document").ready(function() {
+  $("a.skills").click(function(e) {
+    e.preventDefault();
+    var link = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(link).offset().top
+      },
+      1000
+    );
+  });
+});
+
+$("document").ready(function() {
+  $("a.portfolio").click(function(e) {
+    e.preventDefault();
+    var link = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(link).offset().top
+      },
+      1000
+    );
+  });
+});
